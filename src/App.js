6f2efdcabaac94594189af114edcb44e869ca561
joshdelmonte <joshdelmonte@gmail.com>
2023-01-13 20:23:14 -0700

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import logo from './logo.svg';
+import './App.css';
+
+//Page Imports
+import Navbar from './Components/Navbar'
+// import Home from "./Pages/Home"
+// import Footer from './Components/Footer'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <Navbar />
+      </header>
+      {/* Pulled out Home and Footer component from the header element */}
+      {/* <Home /> */}
+      {/* <Footer /> */}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Pages/Home/index.js
@@ -0,0 +1,12 @@
+import React, {} from "react";
+
+function MyHomePage()
+{
+  return (
+    <div>
+      <h1>Home Page</h1>
+    </div>
+  );
+}
+
+export default MyHomePage;
