b1cefdf63a5094dc57533ef8d3b33c9080c34c84
joshdelmonte <joshdelmonte@gmail.com>
2023-01-14 13:23:12 -0700

    Added some code for Resume and a link to said resume

--- src/App.js
+++ src/App.js
@@ -5,6 +5,14 @@ import './App.css';
 import Navbar from './Components/Navbar'
 // import Home from "./Pages/Home"
 // import Footer from './Components/Footer'
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Home from './home';
+import About from './about';
+import Portfolio from './portfolio';
+import Contact from './contact';
+import Resume from './resume';
+import Footer from './footer';
 
 function App() {
   return (
@@ -13,9 +21,14 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <Navbar />
       </header>
-      {/* Pulled out Home and Footer component from the header element */}
-      <Home /> 
-      <Footer />
+      <Switch>
+        <Route path="/" exact component={About} />
+        <Route path="/portfolio" component={Portfolio} />
+        <Route path="/contact" component={Contact} />
+        <Route path="/resume" component={Resume} />
+      </Switch>
+      <Footer /> 
+
     </div>
   );
 }
---
+++ src/Pages/Resume/resume.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+
+funtion MyResume() {
+    return (
+        <div>
+            <Container>
+                
+                    <link rel="#resume" href="https://bold.pro/my/joshua-delmonte" />Resume
+
+            </Container>
+        </div>
+    )
+}
+
+export default MyResume;
