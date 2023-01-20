26abc2f3049ca12dbaf4e6a22483786427acd6b1
joshdelmonte <joshdelmonte@gmail.com>
2023-01-20 09:18:04 -0700

    Adjusting links and images in Portfolio

--- src/Pages/Portfolio/index.js
+++ src/Pages/Portfolio/index.js
@@ -5,7 +5,8 @@ import {
   Button,
   Card
 } from 'react-bootstrap';
-import sampleWeb from '../../Assets/images/profile_picture.jpg';
+import sampleWeb from '../../Assets/images/Previous-additions.png';
+import document from '../../Assets/images/document-image.jpg';
 import './style.css';
 
 const Portfolio = () => {
@@ -15,15 +16,15 @@ const Portfolio = () => {
 
       <Container className="display-flex">
         <Row className="d-flex flex-wrap">
-          <Card className='project-card'>
-            <Card.Img src={sampleWeb} />
+          <Card className='project-card' style={{ maxWidth: '100px' }}>
+            <Card.Img src={document} fluid alt='...'/>
             <Card.Body>
               <Card.Title>Sample Web Development</Card.Title>
               <Card.Text>
                 Some quick example text to build on the card title and make up the
                 bulk of the card's content.
               </Card.Text>
-              <Button variant="primary" href="https://joshdelmonte.github.io/Preliminary-Sample-Web-Development/">Go somewhere</Button>
+              <Button variant="primary" href="https://joshdelmonte.github.io/Preliminary-Sample-Web-Development/" >~Click here for the link~</Button>
             </Card.Body>
           </Card>
           <Card className='project-card'>
@@ -34,45 +35,21 @@ const Portfolio = () => {
                 Some quick example text to build on the card title and make up the
                 bulk of the card's content.
               </Card.Text>
-              <Button variant="primary" href="https://joshdelmonte.github.io/Preliminary-Sample-Web-Development/">Go somewhere</Button>
+              <Button variant="primary" href="https://angi-adema.github.io/Movie-Finder/">~Click here for the link~</Button>
             </Card.Body>
           </Card>
           <Card className='project-card'>
-            <Card.Img src={sampleWeb} />
+            <Card.Img src={document} />
             <Card.Body>
               <Card.Title>Sample Web Development</Card.Title>
               <Card.Text>
                 Some quick example text to build on the card title and make up the
                 bulk of the card's content.
               </Card.Text>
-              <Button variant="primary" href="https://joshdelmonte.github.io/Preliminary-Sample-Web-Development/">Go somewhere</Button>
+              <Button variant="primary" href="https://joshdelmonte.github.io/Preliminary-Sample-Web-Development/">~Click here for the link~</Button>
             </Card.Body>
           </Card>
-          {/* <a href="#">
-            <img
-              src="project-1.jpg"
-              alt="Project 1"
-              className="img-thumbnail mr-3 mb-3"
-            />
-            <p>Sample Web Development</p>
-            <a href="https://joshdelmonte.github.io/Preliminary-Sample-Web-Development/"><button type="button">Previous Works</button></a>
-            <p>Project 1</p>
-          </a> */}
-          {/* <a href="https://angi-adema.github.io/Movie-Finder/"><button type="button">Previous Works</button></a> */}
-          {/* <p>Project 2: Veggiebook</p>
-        
-        <a href="#">
-          <img
-            src="project-4.jpg"
-            alt="Project 4"
-            className="img-thumbnail mr-3 mb-3"
-          />
-          <p>Project 4</p>
-        </a>
-        <a href="#">
-          <img src="project-5.jpg" alt="Project 5" className="img-thumbnail mr-3 mb-3" />
-          <p>Project 5</p>
-        </a> */}
+         
         </Row>
       </Container>
     </>
