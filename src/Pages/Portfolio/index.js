0c11e8274c686fd7c885dbfb6a1c0d109cc39a3b
joshdelmonte <joshdelmonte@gmail.com>
2023-01-20 11:31:18 -0700

    Adjusted links in Portfolio

--- src/Pages/Portfolio/index.js
+++ src/Pages/Portfolio/index.js
@@ -21,8 +21,7 @@ const Portfolio = () => {
             <Card.Body>
               <Card.Title>Sample Web Development</Card.Title>
               <Card.Text>
-                Some quick example text to build on the card title and make up the
-                bulk of the card's content.
+                This was the first of many projects I have worked on. It was a simple.
               </Card.Text>
               <Button variant="primary" href="https://joshdelmonte.github.io/Preliminary-Sample-Web-Development/" >~Click here for the link~</Button>
             </Card.Body>
@@ -30,10 +29,10 @@ const Portfolio = () => {
           <Card className='project-card'>
             <Card.Img src={sampleWeb} />
             <Card.Body>
-              <Card.Title>Sample Web Development</Card.Title>
+              <Card.Title>Movie Finder</Card.Title>
               <Card.Text>
-                Some quick example text to build on the card title and make up the
-                bulk of the card's content.
+                This was my first project! It was a simple movie finder app that uses the OMDB API. We had started with 5 people, and it was a time when
+                people wer starting to drop the class, and we had lost 2 of our team mates. The three of us that stayed gave our best effort to finish.
               </Card.Text>
               <Button variant="primary" href="https://angi-adema.github.io/Movie-Finder/">~Click here for the link~</Button>
             </Card.Body>
@@ -41,12 +40,11 @@ const Portfolio = () => {
           <Card className='project-card'>
             <Card.Img src={document} />
             <Card.Body>
-              <Card.Title>Sample Web Development</Card.Title>
+              <Card.Title>Veggiebook</Card.Title>
               <Card.Text>
-                Some quick example text to build on the card title and make up the
-                bulk of the card's content.
+                This was my second project! We were designin a social media app that would act like a mix between a farmers market, and a social media app more so like Facebook, or Craigslist. Our intention was to inspire people to go local and buy from local farmers. We had 4 people in our group, and we all worked together to make this project a success.
               </Card.Text>
-              <Button variant="primary" href="https://joshdelmonte.github.io/Preliminary-Sample-Web-Development/">~Click here for the link~</Button>
+              <Button variant="primary" href="https://hidden-lake-21615.herokuapp.com">~Click here for the link~</Button>
             </Card.Body>
           </Card>
          
