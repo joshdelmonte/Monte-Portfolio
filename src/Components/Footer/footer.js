57cfa36d5a49553e0f23f828317ff9c092c9c257
joshdelmonte <joshdelmonte@gmail.com>
2023-02-19 17:14:00 -0700

    Altering profile CSS

--- src/Components/Footer/footer.js
+++ src/Components/Footer/footer.js
@@ -10,12 +10,12 @@ function Footer() {
             <h3>Contact Us</h3>
             <ul>
               <li id="email">Email: joshdelmonte@gmail.com</li>
-              <li id="phone">Phone: 555-555-5555</li>
+              <li id="phone">Phone: 719-360-0274</li>
             </ul>
           </Col>
           <Col xs={12} md={4}>
             <h3>Resume</h3>
-            <a href="path/to/your-resume.pdf">Download my resume</a>
+            <a href="https://1drv.ms/b/s!AhDbzGZkV_bainR6XblheiahgX_5?e=JZ3b2E">Resume</a>
           </Col>
           <Col xs={12} md={4}>
             <h3>Social Media</h3>
--- src/Pages/Resume/index.js
+++ src/Pages/Resume/index.js
@@ -8,7 +8,6 @@ function Resume() {
             <Container>
                 <a href="https://1drv.ms/b/s!AhDbzGZkV_bainR6XblheiahgX_5?e=JZ3b2E" target="_blank" rel="noopener noreferrer">
                     <Button variant="primary">  View Resume<FontAwesomeIcon icon="fa-light fa-file-user" />
-                    
                     </Button>
                 </a>
             </Container>
