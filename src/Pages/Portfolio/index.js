fa79a2241997448cd701aec0daa7eceee50b1a6b
joshdelmonte <joshdelmonte@gmail.com>
2023-02-21 17:31:23 -0700

    Got first image in portfolio to appropriate size

--- src/Pages/Portfolio/index.js
+++ src/Pages/Portfolio/index.js
@@ -1,75 +1,59 @@
 import React from 'react';
-import { Container, Row, Carousel, Button } from 'react-bootstrap';
+import {
+  Container,
+  Row,
+  Button,
+  Card
+} from 'react-bootstrap';
 import sampleWeb from '../../Assets/images/Previous-additions.png';
 import document from '../../Assets/images/document-image.jpg';
 import './style.css';
+import Accordion from 'react-bootstrap/Accordion';
 
 const Portfolio = () => {
+
   return (
     <>
+
       <Container className="display-flex">
         <Row className="d-flex flex-wrap justify-content-center">
-          <Carousel>
-            <Carousel.Item>
-              <img src={sampleWeb} alt="Sample Web Development" />
-              <Carousel.Caption>
-                <h3>Sample Web Development</h3>
-                <p>
-                  This was the first of many projects I have worked on. It was a
-                  simple.
-                </p>
-                <Button
-                  variant="primary"
-                  href="https://joshdelmonte.github.io/Preliminary-Sample-Web-Development/"
-                >
-                  ~Click here for the link~
-                </Button>
-              </Carousel.Caption>
-            </Carousel.Item>
-
-            <Carousel.Item>
-              <img src={document} alt="Movie Finder" />
-              <Carousel.Caption>
-                <h3>Movie Finder</h3>
-                <p>
-                  As a novice project, I developed a movie finder app that utilizes
-                  the OMDB API. Initially, our team consisted of five members;
-                  however, as the project progressed, several classmates withdrew,
-                  leaving only three of us to complete the work. Despite this
-                  setback, we persevered and dedicated ourselves to the task at
-                  hand, ultimately producing a finished product that represented
-                  our best efforts.
-                </p>
-                <Button
-                  variant="primary"
-                  href="https://angi-adema.github.io/Movie-Finder/"
-                >
-                  ~Click here for the link~
-                </Button>
-              </Carousel.Caption>
-            </Carousel.Item>
-
-            <Carousel.Item>
-              <img src={document} alt="Veggiebook" />
-              <Carousel.Caption>
-                <h3>Veggiebook</h3>
-                <p>
-                  This was my second project! We were designin a social media app
-                  that would act like a mix between a farmers market, and a social
-                  media app more so like Facebook, or Craigslist. Our intention
-                  was to inspire people to go local and buy from local farmers. We
-                  had 4 people in our group, and we all worked together to make
-                  this project a success.
-                </p>
-                <Button
-                  variant="primary"
-                  href="https://hidden-lake-21615.herokuapp.com"
-                >
-                  ~Click here for the link~
-                </Button>
-              </Carousel.Caption>
-            </Carousel.Item>
-          </Carousel>
+          <Card className='project-card' style={{ margin: 5 }}>
+            <Card.Img src={sampleWeb}className="samplecard" fluid alt='...'/>
+            <Card.Body>
+              <Card.Title>Sample Web Development</Card.Title>
+              <Accordion>
+              
+                This was the first of many projects I have worked on. It was a simple.
+              </Accordion>
+              <Button variant="primary" href="https://joshdelmonte.github.io/Preliminary-Sample-Web-Development/" >~Click here for the link~</Button>
+              
+            </Card.Body>
+          </Card>
+          <Card className='project-card'>
+            <Card.Img src={document} />
+            <Card.Body>
+              <Card.Title>Movie Finder</Card.Title>
+              <Accordion>
+              <Card.Text>
+              As a novice project, I developed a movie finder app that utilizes the OMDB API. Initially, our team consisted of five members; however, as the project progressed, several classmates withdrew, leaving only three of us to complete the work. Despite this setback, we persevered and dedicated ourselves to the task at hand, ultimately producing a finished product that represented our best efforts.
+              </Card.Text>
+              </Accordion>
+              <Button variant="primary" href="https://angi-adema.github.io/Movie-Finder/">~Click here for the link~</Button>
+            </Card.Body>
+          </Card>
+          <Card className='project-card'>
+            <Card.Img src={document} />
+            <Card.Body>
+              <Card.Title>Veggiebook</Card.Title>
+              <Accordion>
+              <Card.Text>
+                This was my second project! We were designin a social media app that would act like a mix between a farmers market, and a social media app more so like Facebook, or Craigslist. Our intention was to inspire people to go local and buy from local farmers. We had 4 people in our group, and we all worked together to make this project a success.
+              </Card.Text>
+              </Accordion>
+              <Button variant="primary" href="https://hidden-lake-21615.herokuapp.com">~Click here for the link~</Button>
+            </Card.Body>
+          </Card>
+         
         </Row>
       </Container>
     </>
@@ -77,3 +61,4 @@ const Portfolio = () => {
 };
 
 export default Portfolio;
+
